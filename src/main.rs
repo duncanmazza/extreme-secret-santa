d041bf1a32ad7f527443750804893946ea2037dc
duncanmazza <dmazza@olin.edu>
2025-02-08 23:29:54 -0800

    Support SSG

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 use ::extreme_secret_santa::obf::deobfuscate;
 use dioxus::prelude::*;
 use std::collections::HashMap;
@@ -161,12 +162,53 @@ lazy_static::lazy_static! {
     static ref PASSWORD: String = deobfuscate("06.1;.04");
 }
 
+// ANCHOR: main
 fn main() {
-    dioxus::launch(App);
+    dioxus::LaunchBuilder::new()
+        // Set the server config only if we are building the server target
+        .with_cfg(server_only! {
+            ServeConfig::builder()
+                // Enable incremental rendering
+                .incremental(
+                    IncrementalRendererConfig::new()
+                        // Store static files in the public directory where other static assets like wasm are stored
+                        .static_dir(
+                            std::env::current_exe()
+                                .unwrap()
+                                .parent()
+                                .unwrap()
+                                .join("public")
+                        )
+                        // Don't clear the public folder on every build. The public folder has other files including the wasm
+                        // binary and static assets required for the app to run
+                        .clear_cache(false)
+                )
+                .enable_out_of_order_streaming()
+        })
+        .launch(app);
+}
+// ANCHOR_END: main
+
+fn app() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+// The server function at the endpoint "static_routes" will be called by the CLI to generate the list of static
+// routes. You must explicitly set the endpoint to `"static_routes"` in the server function attribute instead of
+// the default randomly generated endpoint.
+#[server(endpoint = "static_routes")]
+async fn static_routes() -> Result<Vec<String>, ServerFnError> {
+    // The `Routable` trait has a `static_routes` method that returns all static routes in the enum
+    Ok(Route::static_routes()
+        .iter()
+        .map(ToString::to_string)
+        .collect())
 }
 
 #[component]
-fn App() -> Element {
+fn Index() -> Element {
     let answers = use_signal(|| HashMap::<i32, String>::new());
     let password = PASSWORD.as_str();
 
@@ -279,4 +321,12 @@ fn QuestionCard(question: Question, answers: Signal<HashMap<i32, String>>) -> El
             }
         }
     }
+}
+
+// ANCHOR: static_routes
+#[derive(Routable, Clone, PartialEq)]
+pub enum Route {
+    // Any routes with no dynamic segments in your router will be included in the static routes list
+    #[route("/extreme-secret-santa")]
+    Index {},
 }
